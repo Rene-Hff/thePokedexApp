3ff2c6479ecc72ca487a59a2137b8ce3c7033d4f
Rene Hoff <hoff.rene@web.de>
2026-07-16 13:35:41 +0200

    get data from api for each pokemon

--- script.js
+++ script.js
@@ -1,43 +1,84 @@
-const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=40&offset=0"
-const pokeArray = [];
-async function getPokemons(){
-    let response = await fetch(POKE_API_URL);
+const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon" // get name and url
+const dataArray = [];
+const detailsDataArray = []; 
+const POKE_API_OFFSET = 0;
+const POKE_API_LIMIT = 40;
+const bulbasaurURL = "https://pokeapi.co/api/v2/pokemon/1/" // URL for bulbasaur
+
+
+
+
+
+async function getPokemons(){ 
+    let response;
+        try{
+            response = await fetch(POKE_API_URL + `?limit=${POKE_API_LIMIT}&0ffset=${POKE_API_OFFSET}`);
+        } catch (error){
+            console.log(error);
+        }
+    let responseAsJson = await response.json();
+    let array = Object.keys(responseAsJson.results);
+        for (let index = 0; index < array.length; index++) { // push the url keys into the global array named dataArray
+            dataArray.push(
+                {
+                    url : responseAsJson.results[index].url
+                })
+        }
+    fetchUrl();
+    console.log(dataArray);
+}
+async function fetchUrl(){
+let response;
+let loopArray = Object.keys(dataArray);
+        for (let index = 0; index < dataArray.length; index++) {        
+            try{
+                response = await fetch(dataArray[index].url);
+            } catch(error){
+                console.log(error);
+            }
+        let responseAsJson = await response.json();
+        detailsDataArray.push({
+                details : responseAsJson
+        })
+   }
+    renderPokemons();
+    console.log(detailsDataArray);
+}
+
+// fetch for a single pokemon to get its atributes
+async function getSinglePokemon(){ 
+    let singleResponse = await fetch(bulbasaurURL);
     try{
-        response = await fetch(POKE_API_URL);
+        response = await fetch(bulbasaurURL);    
     } catch (error){
     console.log(error);
     }
-    let responseAsJson = await response.json();
-    console.log(responseAsJson.results); //  console.log(responseAsJson.results[6].name) -> output "squirtle"
-    return responseAsJson.results // returns the array of 40 Pokemons
+    let singleResponseAsJson = await response.json();
+    console.log(singleResponseAsJson); // with .moves you get the moves object and its keys and values 
+    return singleResponseAsJson;
 }
-async function renderPokemon(index){
-let pokeResponse = await getPokemons();
-let workArray = Object.keys(pokeResponse);
-document.getElementById('pokemonlist').innerHTML = "";
 
-    for (let index = 0; index < workArray.length; index++) {
-        pokeArray.push(
-            {
-              name : pokeResponse[index].name
-              // add the URL 
-            }
-        )
-        document.getElementById('pokemonlist').innerHTML += getTemplate(index); // important to check the scopes!!
+
+async function renderPokemons(){
+document.getElementById('pokemonlist').innerHTML = "";
+let loopArray = Object.keys(dataArray);
+    for (let index = 0; index < loopArray.length; index++) {
+        document.getElementById('pokemonlist').innerHTML += getTemplate(index);   
     }
 }
+
 
 function getTemplate(index){
     return `
     <div class="template_box">
-        <h4>${pokeArray[index].name}</h4>
-    </div>
-`
+        <h3>#${detailsDataArray[index].details.id} | ${detailsDataArray[index].details.name}</h3>
+    </div>`
 }
 
 
 function init(index){
 getPokemons();
-renderPokemon(index);
+getSinglePokemon();
+renderPokemons();
 }
 
