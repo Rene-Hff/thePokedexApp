3dcee0f0fb9d5cb6f88b3ce92f08c96e28323510
Rene Hoff <hoff.rene@web.de>
2026-07-18 14:53:46 +0200

    get images and types of each pokemon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon" // get name and url
-const dataArray = [];
-const detailsDataArray = []; 
+const dataArray = []; // only url 
+const detailsDataArray = []; // Object keys and values
+const typesArray = [];
 const POKE_API_OFFSET = 0;
-const POKE_API_LIMIT = 40;
+const POKE_API_LIMIT = 50;
 const bulbasaurURL = "https://pokeapi.co/api/v2/pokemon/1/" // URL for bulbasaur
 
 
@@ -33,18 +34,35 @@ let loopArray = Object.keys(dataArray);
         for (let index = 0; index < dataArray.length; index++) {        
             try{
                 response = await fetch(dataArray[index].url);
-            } catch(error){
+            }   catch(error){
                 console.log(error);
             }
         let responseAsJson = await response.json();
-        detailsDataArray.push({
+                detailsDataArray.push({
                 details : responseAsJson
         })
    }
     renderPokemons();
+    //fetchTypesUrl();
     console.log(detailsDataArray);
 }
 
+/*async function fetchTypesUrl(){
+let response;
+let loopArray = Object.keys(dataArray);
+        for(let index = 0; index < dataArray.length; index++){
+            try{
+                response = await fetch(detailsDataArray[index].details.types[index].type.url);
+            }   catch(error){
+                console.log(error);
+            }
+            let responseAsJson = await response.json();
+                typesArray.push({
+                types : responseAsJson
+        })
+        }
+}
+*/
 // fetch for a single pokemon to get its atributes
 async function getSinglePokemon(){ 
     let singleResponse = await fetch(bulbasaurURL);
@@ -71,7 +89,9 @@ let loopArray = Object.keys(dataArray);
 function getTemplate(index){
     return `
     <div class="template_box">
-        <h3>#${detailsDataArray[index].details.id} | ${detailsDataArray[index].details.name}</h3>
+        <h4>#${detailsDataArray[index].details.id} ${detailsDataArray[index].details.name.toUpperCase()}</h4>
+        <img class="zoom img" src ="${detailsDataArray[index].details.sprites.front_default}"
+        <p>${detailsDataArray[index].details.types[0].type.name}</p>
     </div>`
 }
 
@@ -79,6 +99,5 @@ function getTemplate(index){
 function init(index){
 getPokemons();
 getSinglePokemon();
-renderPokemons();
 }
 
