77bf005a07f22ea8cc5a0fc36a28bd66478604c0
Rene Hoff <hoff.rene@web.de>
2026-07-30 21:30:48 +0200

    added several functions to fetch for evolution chain

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon" // get name and url
-const dataArray = []; // only url 
-const detailsDataArray = []; // Object keys and values
+const speciesURl   = "https://pokeapi.co/api/v2/pokemon-species";
+const dataArray = []; // pokemon urls
+const detailsDataArray = []; // Main-Object keys and values
+const specArray = []; // species urls
+const evoChainArray =[]; // Objects to fetch for evo-chain
+const evoChainDataArray = [];
 const POKE_API_OFFSET = 0;
 const POKE_API_LIMIT = 50;
 const bulbasaurURL = "https://pokeapi.co/api/v2/pokemon/1/" // URL for bulbasaur
-const typeURL =  "https://pokeapi.co/api/v2/type" // URL for all types of pokemon
 const dialogRef = document.getElementById('cardDialog');
 
 
-
-
-
-async function getPokemons(){ 
+async function getPokemons(){ //fetching the poke-api to get data and pushing the url in an array
     let response;
         try{
             response = await fetch(POKE_API_URL + `?limit=${POKE_API_LIMIT}&0ffset=${POKE_API_OFFSET}`);
@@ -29,9 +29,10 @@ async function getPokemons(){
     fetchUrl();
     console.log(dataArray);
 }
-async function fetchUrl(){
-let response;
-let loopArray = Object.keys(dataArray);
+
+async function fetchUrl(){ // fetching the dataArray to get the details of each pokemon
+    let response;
+    let loopArray = Object.keys(dataArray);
         for (let index = 0; index < dataArray.length; index++) {        
             try{
                 response = await fetch(dataArray[index].url);
@@ -44,11 +45,61 @@ let loopArray = Object.keys(dataArray);
         })
    }
     renderPokemons();
-    console.log(detailsDataArray);
+    fetchSpecies();
 }
 
+async function fetchSpecies(){
+    let speciesData;
+            try{
+                response = await fetch(speciesURl + `?limit=${POKE_API_LIMIT}&0ffset=${POKE_API_OFFSET}`); 
+            } catch(error) {
+                console.log(error);
+            }
+        let responseAsJson = await response.json();
+        let array = Object.keys(responseAsJson.results);
+            for (let index = 0; index < array.length; index++) {
+                specArray.push({
+                    speciesUrl : responseAsJson.results[index].url
+                })
+            }
+    console.log(specArray)
+    fetchForEvolutionStats();
+ }
 
+async function fetchForEvolutionStats(){ // to get keys of the evo-chain
+let response;
+let loopArray = Object.keys(specArray);
+    for (let index = 0; index < specArray.length; index++) {
+        try{
+            response = await fetch(specArray[index].speciesUrl); // response recived an Object with key-value pairs to fetch for the evolution chain 
+        }   catch(error){
+            console.log(error);
+        }
+    let responseAsJson = await response.json();
+        evoChainArray.push({
+        evoKey : responseAsJson     // evoKey = Object 
+        })
+    }
+    console.log(evoChainArray); // evoChainArray includes  Objects from url fetch of species
+    fetchForEvoChainData();
+}
 
+async function fetchForEvoChainData(){
+let response;
+let loopArray = Object.keys(evoChainArray);
+    for (let index = 0; index < evoChainArray.length; index++) {
+            try{
+                response = await fetch(evoChainArray[index].evoKey.evolution_chain.url);
+            }   catch(error){
+                console.log(error);
+            }
+    let responseAsJson = await response.json();
+        evoChainDataArray.push({
+        chainKey: responseAsJson
+        })
+    }
+    console.log(evoChainDataArray);
+}
 // fetch for a single pokemon to get its atributes
 async function getSinglePokemon(){ 
     let singleResponse = await fetch(bulbasaurURL);
@@ -62,25 +113,23 @@ async function getSinglePokemon(){
     return singleResponseAsJson;
 }
 
-
-async function renderPokemons(){
-document.getElementById('pokemonList').innerHTML = "";
-let loopArray = Object.keys(dataArray);
-    for (let index = 0; index < loopArray.length; index++) {
-        document.getElementById('pokemonList').innerHTML += getTemplate(index);   
-    }
+async function renderPokemons(){ // looping through the dataArray to return the templates for each pokemon
+    document.getElementById('pokemonList').innerHTML = "";
+    let loopArray = Object.keys(dataArray);
+        for (let index = 0; index < loopArray.length; index++) {
+            document.getElementById('pokemonList').innerHTML += getTemplate(index);   
+        }
 }
 
-
-function getTemplate(index, typeIndex){ 
+function getTemplate(index, typeIndex){ // Template for Card with name, img and types 
     let types;
         types = renderTypes(index, typeIndex);
     return `
-    <button type="button" class="template_box ${detailsDataArray[index].details.types[0].type.name}" onclick="openDialog(${index})"> 
-        <h2>#${detailsDataArray[index].details.id} ${detailsDataArray[index].details.name.toUpperCase()}</h2>
-        <img class="zoom img" src ="${detailsDataArray[index].details.sprites.front_default}"/>
-        ${types} 
-    </button>`
+        <button type="button" class="template_box ${detailsDataArray[index].details.types[0].type.name}" onclick="openDialog(${index})"> 
+            <h2>#${detailsDataArray[index].details.id} ${detailsDataArray[index].details.name.toUpperCase()}</h2>
+            <img class="zoom img" src ="${detailsDataArray[index].details.sprites.front_default}"/>
+            ${types} 
+        </button>`
 }
 
 function openDialog(index){
@@ -93,12 +142,12 @@ function bubblingPrevention(event){
 }
 
 function renderDialogCard(index){
-let diaCont = document.getElementById('dialogContentBox');
-let btnDiv = document.getElementById('buttonsDiv');
-let info = document.getElementById('infoCard');
-    diaCont.innerHTML = getTemplate(index);
-    btnDiv.innerHTML = renderDialogBtns(index);
-    info.innerHTML = renderInfo(index);
+    let diaCont = document.getElementById('dialogContentBox');
+    let btnDiv = document.getElementById('buttonsDiv');
+    let info = document.getElementById('infoCard');
+        diaCont.innerHTML = getTemplate(index);
+        btnDiv.innerHTML = renderDialogBtns(index);
+        info.innerHTML = renderInfo(index);
 }
 
 function renderDialogBtns(index){
@@ -108,16 +157,17 @@ return `
     <button class="buttonStyles">Evo</button>             
 `
 }
+
 function renderInfo(index){
-let info = document.getElementById('infoCard');
-document.getElementById('progressCard').style = "display: none";
-document.getElementById('infoCard').style = "";
-info.innerHTML = `
-    <p>Weight: ${detailsDataArray[index].details.weight}</p>
-    <p>Height: ${detailsDataArray[index].details.height}</p>
-    <p>Base Experience: ${detailsDataArray[index].details.base_experience}</p>
-    <p>Abilities: ${detailsDataArray[index].details.abilities[0].ability.name} & ${detailsDataArray[index].details.abilities[1].ability.name}</p>
-`
+    let info = document.getElementById('infoCard');
+    document.getElementById('progressCard').style = "display: none";
+    document.getElementById('infoCard').style = "";
+    info.innerHTML = `
+        <p>Weight: ${detailsDataArray[index].details.weight}</p>
+        <p>Height: ${detailsDataArray[index].details.height}</p>
+        <p>Base Experience: ${detailsDataArray[index].details.base_experience}</p>
+        <p>Abilities: ${detailsDataArray[index].details.abilities[0].ability.name} & ${detailsDataArray[index].details.abilities[1].ability.name}</p>
+    `
 return info.innerHTML
 }
 
@@ -126,34 +176,41 @@ function renderProgress(index){
     document.getElementById('infoCard').style = "display: none";
     document.getElementById('progressCard').style = "";
     progress.innerHTML = `
-    <div class="progressBarDiv">
-        <div class="bar" style="height: 15px; width: 60%"></div>
-    </div>
-    <div class="progressBarDiv">
-        <div class="bar" style="height: 15px; width: 40%"></div>
-    </div>
-    <div class="progressBarDiv">
-        <div class="bar" style="height: 15px; width: 20%"></div>
-    </div>
-    <div class="progressBarDiv">
-        <div class="bar" style="height: 15px; width: 20%"></div>
-    </div>
-`
+        <div class="progressBarDiv">
+            <span>${detailsDataArray[index].details.stats[0].stat.name}:</span>
+            <div class="bar" style="height: 15px; width: ${detailsDataArray[index].details.stats[0].base_stat}%"></div>
+        </div>
+        <div class="progressBarDiv">
+            <span>${detailsDataArray[index].details.stats[1].stat.name}:</span>
+            <div class="bar" style="height: 15px; width: ${detailsDataArray[index].details.stats[1].base_stat}%"></div>
+        </div>
+        <div class="progressBarDiv">
+            <span>${detailsDataArray[index].details.stats[2].stat.name}:</span>
+            <div class="bar" style="height: 15px; width: ${detailsDataArray[index].details.stats[2].base_stat}%"></div>
+        </div>
+        <div class="progressBarDiv">
+            <span>${detailsDataArray[index].details.stats[3].stat.name}:</span>
+            <div class="bar" style="height: 15px; width: ${detailsDataArray[index].details.stats[3].base_stat}%"></div>
+        </div>
+    `
 }
 
 function renderTypes(index, typeIndex){
-let types = ""; // need to declare with  to prevent undefined
-for (let typeIndex = 0; typeIndex < detailsDataArray[index].details.types.length; typeIndex++) { 
-      // typeIndex is the index of the inside Array of detailDataArray[].details.types[], to get the types of each Pokemon
-         types += `<div class="type-name">${detailsDataArray[index].details.types[typeIndex].type.name}</div>`
-    }
-    return types
+    let types = ""; // need to declare with  to prevent undefined
+        for (let typeIndex = 0; typeIndex < detailsDataArray[index].details.types.length; typeIndex++) { 
+            // typeIndex is the index of the inside Array of detailDataArray[].details.types[], to get the types of each Pokemon
+                types += `<div class="type-name">${detailsDataArray[index].details.types[typeIndex].type.name}</div>`
+            }
+            return types
 }
+
+
 function closeDialog(){
-dialogRef.close();
+    dialogRef.close();
 }
+
 function init(index){
-getPokemons();
-getSinglePokemon();
+    getPokemons();
+    getSinglePokemon();
 }
 
